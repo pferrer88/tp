737225fa55b2cff3eab0bd26304703fb1a71eb92
Pedro F <pferrer88@gmail.com>
2012-09-25 16:15:24 -0430

    more advanced homapge tune query with own tunes and of followees ordered chronologycally

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # @users = User.all
-    @tunes = Tune.where(:user_id => current_user.followees(User))
+    @tunes = Tune.where(:user_id => (current_user.followees(User).push(current_user))).order('created_at DESC')
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tunes = @user.tunes
   end
 
   def follow
